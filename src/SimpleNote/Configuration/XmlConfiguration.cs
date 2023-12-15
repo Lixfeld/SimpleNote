586749c87d4d06e1c22754095118a8f4b3169eac
Lixfeld <lixfeld-git@gmx.de>
2023-12-15 17:28:03 +0100

    Add XmlConfiguration, extensions and config file

---
+++ src/SimpleNote/Configuration/XmlConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SimpleNote.Configuration
+{
+    public class XmlConfiguration
+    {
+        public const string Filename = "Config.xml";
+
+        public double WindowTop { get; set; }
+        public double WindowLeft { get; set; }
+        public double WindowHeight { get; set; }
+        public double WindowWidth { get; set; }
+        public string NoteText { get; set; }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+        private XmlConfiguration()
+        {
+            // Only for XmlSerialzer
+        }
+
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+        public XmlConfiguration(double windowTop, double windowLeft, double windowHeight, double windowWidth, string noteText)
+        {
+            WindowTop = windowTop;
+            WindowLeft = windowLeft;
+            WindowHeight = windowHeight;
+            WindowWidth = windowWidth;
+            NoteText = noteText;
+        }
+
+        public static XmlConfiguration Load()
+        {
+            string text = File.ReadAllText(Filename, Encoding.UTF8);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(XmlConfiguration));
+            return xmlSerializer.DeserializeObject<XmlConfiguration>(text);
+        }
+
+        public void Save()
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(XmlConfiguration));
+            string text = xmlSerializer.SerializeObject(this, indent: true);
+            File.WriteAllText(Filename, text, Encoding.UTF8);
+        }
+    }
+}
---
+++ src/SimpleNote/Configuration/XmlSerializerExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Text;
+
+namespace System.Xml.Serialization
+{
+    /// <summary>
+    /// Extension methods for System.Xml.Serialization.XmlSerializer
+    /// </summary>
+    public static class XmlSerializerExtensions
+    {
+        private class UTF8StringWriter : StringWriter
+        {
+            public override Encoding Encoding => Encoding.UTF8;
+        }
+
+        public static string SerializeObject(this XmlSerializer xmlSerializer, object obj, XmlWriterSettings settings)
+        {
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+
+            if (settings == null)
+                throw new ArgumentNullException(nameof(settings));
+
+            using (StringWriter stringWriter = new UTF8StringWriter())
+            {
+                using (XmlWriter xmlWriter = XmlWriter.Create(stringWriter, settings))
+                {
+                    xmlSerializer.Serialize(xmlWriter, obj);
+                    return stringWriter.ToString();
+                }
+            }
+        }
+
+        public static string SerializeObject(this XmlSerializer xmlSerializer, object obj, bool indent = false)
+        {
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+
+            XmlWriterSettings settings = new XmlWriterSettings()
+            {
+                Indent = indent
+            };
+
+            return xmlSerializer.SerializeObject(obj, settings);
+        }
+
+        public static T DeserializeObject<T>(this XmlSerializer xmlSerializer, string value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentException("Argument is empty", nameof(value));
+
+            using (StringReader stringReader = new StringReader(value))
+            {
+                return (T)xmlSerializer.Deserialize(stringReader)!;
+            }
+        }
+    }
+}
